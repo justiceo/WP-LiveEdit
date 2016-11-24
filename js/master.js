d59ee5ff3616b3c06bc226e2b1d6f1c4897475f2
Justice Ogbonna <ebesco@yahoo.com>
2016-11-23 23:24:58 -0500

    added settings plugin

--- js/master.js
+++ js/master.js
@@ -15,7 +15,7 @@ $(function()
     if ($('#redactor-intro-box').size() !== 0)
     {
 	    $('#redactor').redactor({
-			plugins: ['table', 'video', 'source', 'imagemanager', 'comment'],	
+			plugins: ['table', 'video', 'source', 'imagemanager', 'comment', 'settings'],	
 			imageUpload: '/a/url/here',
 			imagePosition: true,
 			imageResizable: true,
