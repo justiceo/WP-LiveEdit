c99320733f7f69e9b917ffbac8c6dcb71be17792
Justice Ogbonna <ebesco@yahoo.com>
2016-12-30 19:25:57 -0500

    add separate title and body editors

--- medium-editor/main.js
+++ medium-editor/main.js
@@ -1,6 +1,7 @@
 /* custom javascript */
 
 window.setTimeout( function() {
-        new MediumEditor('.editable');
+        new MediumEditor('h2.entry-title');
+        new MediumEditor('.post-content');
 }, 1000);
 console.log("med obj", smartpen_object);
