86391bb44563a4e11e92c57914060f8d37c7f97f
Justice Ogbonna <ebesco@yahoo.com>
2016-12-30 20:37:17 -0500

    included it in the demo

--- medium-editor/main.js
+++ medium-editor/main.js
@@ -2,10 +2,21 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     var headerE = new MediumEditor('h2.entry-title');
-    var bodyE = new MediumEditor('.post-content');
+    var autolist = new AutoList();
+    var editor = new MediumEditor('.post-content', {
+        buttonLabels: 'fontawesome',
+        extensions: {
+            'autolist': autolist
+        },
+        toolbar: {
+            buttons: ['h1', 'h2', 'bold', 'italic', 'quote', 'pre', 'unorderedlist','orderedlist', 'justifyLeft', 'justifyCenter', 'anchor']
+        }
+
+    }),
+    cssLink = document.getElementById('medium-editor-theme');
     $(function () {
         $('.post-content').mediumInsert({
-            editor: bodyE
+            editor: editor
         });
     });
 });
