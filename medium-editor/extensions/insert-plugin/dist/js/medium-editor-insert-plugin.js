6758f25ff1bd7984175ad40ec7df99842f449008
axeldimaria <antwari@fandm.edu>
2017-01-13 15:36:51 -0500

    Added the ability to insert the read more (<!-- more -->) tag on a new line

--- medium-editor/extensions/insert-plugin/dist/js/medium-editor-insert-plugin.js
+++ medium-editor/extensions/insert-plugin/dist/js/medium-editor-insert-plugin.js
@@ -1,8 +1,8 @@
-/*! 
+/*!
  * medium-editor-insert-plugin v2.4.0 - jQuery insert plugin for MediumEditor
  *
  * http://linkesch.com/medium-editor-insert-plugin
- * 
+ *
  * Copyright (c) 2014 Pavel Linkesch (http://linkesch.com)
  * Released under the MIT license
  */
@@ -174,7 +174,8 @@ this["MediumInsert"]["Templates"]["src/js/templates/images-toolbar.hbs"] = Handl
             enabled: true,
             addons: {
                 images: true, // boolean or object containing configuration
-                embeds: true
+                embeds: true,
+                readmore: true
             }
         };
 
@@ -2344,5 +2345,108 @@ this["MediumInsert"]["Templates"]["src/js/templates/images-toolbar.hbs"] = Handl
     };
 
 })(jQuery, window, document, MediumEditor.util);
+
+/* Readmore Tag */
+
+; (function ($, window, document, Util, undefined) {
+
+    'use strict';
+
+    /** Default values */
+    var pluginName = 'mediumInsert',
+        addonName = 'Readmore', // first char is uppercase
+        defaults = {
+            label: '<span class="fa fa-ellipsis-h"></span>'
+        };
+
+    /**
+     * Readmore object
+     *
+     * Sets options, variables and calls init() function
+     *
+     * @constructor
+     * @param {DOM} el - DOM element to init the plugin on
+     * @param {object} options - Options to override defaults
+     * @return {void}
+     */
+
+    function Readmore (el, options) {
+        this.el = el;
+        this.$el = $(el);
+        this.templates = window.MediumInsert.Templates;
+        this.core = this.$el.data('plugin_'+ pluginName);
+
+        this.options = $.extend(true, {}, defaults, options);
+
+        this._defaults = defaults;
+        this._name = pluginName;
+
+        this.init();
+    }
+
+    /**
+     * Initialization
+     *
+     * @return {void}
+     */
+
+    Readmore.prototype.init = function () {
+        this.events();
+    };
+
+    /**
+     * Event listeners
+     *
+     * @return {void}
+     */
+
+    Readmore.prototype.events = function () {
+
+    };
+
+    /**
+     * Get the Core object
+     *
+     * @return {object} Core object
+     */
+    Readmore.prototype.getCore = function () {
+        return this.core;
+    };
+
+    /**
+     * Add custom content
+     *
+     * This function is called when user click on the addon's icon
+     *
+     * @return {void}
+     */
+
+    Readmore.prototype.add = function () {
+        var $place = this.$el.find('.medium-insert-active');
+
+        // Fix #132
+        // Make sure that the content of the paragraph is empty and <br> is wrapped in <p></p> to avoid Firefox problems
+        $place.html(this.templates['src/js/templates/core-empty-line.hbs']().trim());
+
+        if ($place.is('p')) {
+            $place.replaceWith('<!-- more --> <span contenteditable="false" style="color:grey">&#x25cf  &nbsp &nbsp &#x25cf  &nbsp&nbsp &#x25cf  </span> <br>');
+            $place = this.$el.find('.medium-insert-active');
+        }
+
+        this.core.hideButtons();
+    };
+
+
+    /** Addon initialization */
+
+    $.fn[pluginName + addonName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName + addonName)) {
+                $.data(this, 'plugin_' + pluginName + addonName, new Readmore(this, options));
+            }
+        });
+    };
+
+})(jQuery, window, document);
 
 }));
