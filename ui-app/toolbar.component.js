fc995e72ef9b9662d7d1888527a4a0247ed55c73
Justice Ogbonna <ebesco@yahoo.com>
2017-02-09 11:50:50 -0500

    clean up toolbar, remove refreshing via scope.apply

--- ui-app/toolbar.component.js
+++ ui-app/toolbar.component.js
@@ -4,19 +4,14 @@
 angular.module('le', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ui.bootstrap'])
     .controller('ToolbarCtrl', function($scope, $window, $uibModal, $uibTooltip, $mdDialog, ToolbarService) {
         var toolbar = this;
-        toolbar.buttons = [];
-		toolbar.getButtons = function() {
-            return ToolbarService.getButtons();
-        }
-		console.log("toolbar buttons: ", toolbar.buttons);
-        toolbar.$scope = $scope;
-
+		toolbar.buttons = ToolbarService.getButtons();
+/*
         $scope.$on("le-collection-changed", function() {
-            console.log("collection changed captured by ctrl");
-            toolbar.buttons = ToolbarService.getButtons();
-            $scope.safeApply();
+            console.log("ToolbarCtrl: collection changed...");
+            //toolbar.buttons = ToolbarService.getButtons();
+            //$scope.safeApply();
         });
-
+*/
         $scope.safeApply = function(fn) {
             var phase = $scope.$root.$$phase;
             if (phase == "$apply" || phase == "$digest") {
@@ -27,13 +22,6 @@ angular.module('le', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ui
                 $scope.$apply(fn);
             }
         };
-
-        function noop() { console.log("no op"); }
-
-        function cancelHandler() {
-            console.log("cancel clicked from toolbar ctrl");
-        }
-
     })
     .component('leToolbar', {
         templateUrl: 'toolbar.html',
